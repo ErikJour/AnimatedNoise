4449e171ab596a257c3bd488fb5f58cc1d90b33f
Erik Jourgensen <erikjourgensen@gmail.com>
2026-05-19 06:13:59 -0700

    Starting comb filter implementation

---
+++ plugin/dsp/CombFilter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Erik Jourgensen on 5/19/26.
+//
+
+#include "CombFilter.h"
---
+++ plugin/dsp/CombFilter.h
@@ -0,0 +1,14 @@
+//
+// Created by Erik Jourgensen on 5/19/26.
+//
+
+#ifndef ANIMATEDNOISE_COMBFILTER_H
+#define ANIMATEDNOISE_COMBFILTER_H
+
+
+class CombFilter
+{
+};
+
+
+#endif //ANIMATEDNOISE_COMBFILTER_H
