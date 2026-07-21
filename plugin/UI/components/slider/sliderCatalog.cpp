9204030d7bdea4814fc9bc2dc1bd2e644c7b9f10
Erik Jourgensen <erikjourgensen@gmail.com>
2026-07-21 10:27:07 -0700

    Adding aux envelope

--- plugin/UI/components/slider/sliderCatalog.cpp
+++ plugin/UI/components/slider/sliderCatalog.cpp
@@ -7,22 +7,35 @@ const std::vector<SliderDef>& sliderDefinitions()
 {
 
     static const std::vector<SliderDef> defs = {
+        //==================================================
+        //Noise Level: Radius = 2.0, angle = 0.0
+        //==================================================
         { ParameterID::noiseLevel,
             0.0f,
             MAT_MASTER_GAIN_SLIDER,
-            { 1.5f, 0.35f, -0.9f },
+            { 2.0f, 0.15f, 0.0f },
             0.07f },
+        //==================================================
+        //Noise Level Mod: Radius = 2.0, angle = 0.0, height:
+        //==================================================
 
+        //==================================================
+        //Noise Density: Radius = 2.0, angle = 7.0
+        //==================================================
         { ParameterID::noiseDensity,
             0.0f,
             MAT_NOIS_DENS_SLIDER,
-            { 1.5f, 0.15f, -0.9f },
+                { 1.985f, 0.15f, 0.244f },
             0.07f },
+        //==================================================
+        //Noise Density Mod: Radius = 2.0, angle = 7.0, Height:
+        //==================================================
     };
-
-
-
 
 
     return defs;
 }
+
+//Calculate x and z based on angle:
+//x = radius * cos(angle)
+//z = radiu s* sin(angle)
